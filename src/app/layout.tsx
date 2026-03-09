0a5c8dc2b3e6f2196b38d2b0f9d7815ea7eca0be
unknown <jophin735@gmail.com>
2026-03-08 21:08:12 -0700

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -24,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navigation />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
